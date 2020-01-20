0a30442e1eeb904745fbaba90497d975f8ab4b8a
profawxhawk <bharath17035@iiitd.ac.in>
2020-01-20 16:01:47 +0530

    added skeleton code

--- Assignment1/AES-assignment.py
+++ Assignment1/AES-assignment.py
@@ -1,27 +1,42 @@
 import os
 import math
 from AES import AES
-def calc_size(filename):
-    temp=os.path.getsize(filename)
+
+# function to return size of file in bytes
+
+def calc_size(blocks):
+    temp=(len(blocks)-1)*(16)+(len(blocks[len(blocks)-1]))
     print("Size of file is "+str(temp)+" bytes")
-    if math.ceil(temp/128)==1:
-        print("Generating "+str(math.ceil(temp/128))+" block")
+    if math.ceil(temp/16)==1:
+        print("Generating "+str(math.ceil(temp/16))+" block")
     else:
-        print("Generating "+str(math.ceil(temp/128))+" blocks")
+        print("Generating "+str(math.ceil(temp/16))+" blocks")
+
+# function to return an array of 16-byte elements from a given open file descriptor ( not padded )
 
 def get_blocks(f):
     blocks=[]
     while True:
-        k=f.read(128)
+        k=f.read(16)
         if k:
-            blocks.append(k)
+            blocks.append(str.encode(k))
         else:
             break
     return blocks
+
+# function to pad last element of blocks array so that all the elements in the array have an uniform size of 16 bytes. ( uses pkcs5 padding )
+
+def pad_block_PKCS5(blocks):
+    if len(blocks[len(blocks)-1])!=16:
+        blocks[len(blocks)-1]=(blocks[len(blocks)-1]+bytes([(16-len(blocks))]*(16-len(blocks))))
+
 if __name__ == '__main__':
     filename=input('Enter full path of file')
     f=open(filename,'r')
-    calc_size(filename)
     blocks=get_blocks(f)
+    calc_size(blocks)
+    pad_block_PKCS5(blocks)
     Aes=AES(128,10,blocks)
+    Aes.encrypt()
+    
     
--- Assignment1/AES.py
+++ Assignment1/AES.py
@@ -1,10 +1,61 @@
+import sys
 class AES:
     key_size=0
     rounds=0
-    blocks=[]
+    state_vector=[]
+    blocks_input=[]
+    blocks_output=[]
+    key_list=[]
+    master_key=0
     def __init__(self,key_size,rounds,blocks):
         self.key_size=key_size
         self.rounds=rounds
-        self.blocks=blocks
+        self.blocks_input=blocks
+
+    def convert_to_state(self,input):
+        state=[[],[],[],[]]
+        count=0
+        temp=[i.to_bytes(1, sys.byteorder) for i in input]
+        for i in temp:
+            state[count%4].append(i)
+            count+=1
+        return state
+        
+    def round_key_addition(index):
+        pass
+
+    def substitution_bytes():
+        pass
+
+    def shift_rows():
+        pass
+
+    def mix_columns():
+        pass
+
+
+    
+    def encrypt_block(self,index):
+
+        if(len(self.blocks_input[index])!=16):
+            print("block size not 128 bites. exiting")
+            exit(0)
+
+        self.state_vector=self.convert_to_state(self.blocks_input[index])
+        
+        round_key_addition(0)
+
+        for i in range(1,self.rounds+1):
+            substitution_bytes()
+            shift_rows()
+            if i!=(self.rounds):
+                mix_columns()
+            round_key_addition(i)
+            
+
+
+    def encrypt(self):
+        for i in range(len(self.blocks_input)):
+            self.encrypt_block(i)
 
 
